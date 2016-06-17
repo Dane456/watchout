3f63ddac094b15c70a2cc9e32902eee9c05cbe1a
Dane456 <dane.e.jordan@gmail.com>
2016-06-17 15:25:04 -0700

    implement asteroids

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,34 @@
 // start slingin' some d3 here.
+var boardSpecs = {
+  height: 700,
+  width: 700
+};
+var numEnemies = 30;
+
+var board = d3.select('body').append('svg')
+  .attr('width', boardSpecs.width)
+  .attr('height', boardSpecs.height);
+
+var asteroids = _.range(0, numEnemies).map(function(i) {
+  return {
+    id: i,
+    x: Math.random() * boardSpecs.width,
+    y: Math.random() * boardSpecs.height
+  };
+});
+
+board.selectAll('circle')
+  .data(asteroids, function(d) { return d.id; })
+  .enter()
+  .append('image')
+  .attr('r', 0)
+  .attr('x', function(d) { return d.x; } )
+  .attr('y', function(d) { return d.y; } )
+  .attr('width', '30')
+  .attr('height', '30')
+  .attr('xlink:href', './asteroid.png')
+  // .style({'href': 'url(./asteroid.png)'})
+  .transition(1000).attr('r', 10);
+
+
+
