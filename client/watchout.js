3a8ed99853efca12c5ce6ed1947b915e400da8e2
Dane456 <dane.e.jordan@gmail.com>
2016-06-17 18:02:37 -0700

    detect collision between player and enemies while enemies are moving

--- client/watchout.js
+++ client/watchout.js
@@ -9,6 +9,29 @@ var board = d3.select('body').append('svg')
   .attr('width', boardSpecs.width)
   .attr('height', boardSpecs.height);
 
+var drag = d3.behavior.drag()
+  .origin(function(){
+    var t = d3.select(this);
+    return {x: t.attr('x'), y: t.attr('y')};
+  })
+  .on('drag', function () {
+    d3.select(this)
+      .attr('x', d3.event.x)
+      .attr('y', d3.event.y);
+  });
+
+var $player = board.append('image')
+  .attr('class', 'player')
+  .attr('x', boardSpecs.width / 2)
+  .attr('y', boardSpecs.height / 2)
+  .attr('width', '0')
+  .attr('height', '0')
+  .attr('xlink:href', './player.png')
+  .call(drag)
+  .transition().duration(500)
+  .attr('width', '30')
+  .attr('height', '30');
+
 var makeAsteroids = function() {
   var asteroids = _.range(0, numEnemies).map(function(i) {
     return {
@@ -20,27 +43,56 @@ var makeAsteroids = function() {
   return asteroids;
 };
 
-var placeAsteroids = function(data){
-  var $asteroids = board.selectAll('image')
+var collisionCheck = function (enemy) {
+  var eBox = enemy.getBBox();
+  var enemyLeft = eBox.x;
+  var enemyRight = eBox.x + eBox.width;
+  var enemyTop = eBox.y;
+  var enemyBottom = eBox.y + eBox.height;
+
+  var pBox = $player[0][0].getBBox();
+  var playerLeft = pBox.x;
+  var playerRight = pBox.x + pBox.width;
+  var playerTop = pBox.y;
+  var playerBottom = pBox.y + pBox.height;
+
+  if (enemyLeft < playerRight 
+      && enemyTop < playerBottom
+      && enemyRight > playerLeft
+      && enemyBottom > playerTop 
+    ) {
+    console.log('HIT');
+  }
+};
+
+var placeAsteroids = function(data) {
+  var $asteroids = board.selectAll('.asteroids')
     .data(data, function(d) { return d.id; });
 
   $asteroids 
     .transition().duration(1000)
     .attr('x', function(d) { return d.x; } )
-    .attr('y', function(d) { return d.y; } );
+    .attr('y', function(d) { return d.y; } )
+    .tween('x', function () {
+      return function () { collisionCheck(this); };
+    });
 
   $asteroids.enter()
     .append('image')
+    .attr('class', 'asteroids')
     .attr('x', function(d) { return d.x; } )
     .attr('y', function(d) { return d.y; } )
     .attr('width', '0')
     .attr('height', '0')
-    .transition().duration(500).attr('width', '30').attr('height', '30')
-    .attr('xlink:href', './asteroid.png');
+    .transition().duration(500)
+    .attr('width', '40').attr('height', '40')
+    .attr('xlink:href', './enemy.png');
 };
 
 setInterval(function() {
   placeAsteroids(makeAsteroids());
 }, 2000);
+
+
 
 
